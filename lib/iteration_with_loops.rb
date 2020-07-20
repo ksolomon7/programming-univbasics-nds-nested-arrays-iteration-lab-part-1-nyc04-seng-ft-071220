2ab4048413d56623feb9c671e0e543e47a51dafb
Kalkidan Taye <kalkidan.solomon2@gmail.com>
2020-07-20 14:03:33 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,7 @@ def find_even_values(src)
   while row_index<src.count do
     element_index=0
     while element_index<src.count do
-      if src[row_index][element_index].even
+      if src[row_index][element_index].even?
         p src[row_index][element_index]
       end
       element_index+=1
